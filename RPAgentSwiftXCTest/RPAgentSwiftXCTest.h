98471f43d3ac329c895f591776ed8684487d0617
Sergey Komarov <sergey.komarov@oxagile.com>
2017-08-31 18:46:19 +0300

    Initial Commit

---
+++ RPAgentSwiftXCTest/RPAgentSwiftXCTest.h
@@ -0,0 +1,19 @@
+//
+//  RPAgentSwiftXCTest.h
+//  RPAgentSwiftXCTest
+//
+//  Created by Sergey Komarov on 8/31/17.
+//  Copyright © 2017 Sergey Komarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RPAgentSwiftXCTest.
+FOUNDATION_EXPORT double RPAgentSwiftXCTestVersionNumber;
+
+//! Project version string for RPAgentSwiftXCTest.
+FOUNDATION_EXPORT const unsigned char RPAgentSwiftXCTestVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RPAgentSwiftXCTest/PublicHeader.h>
+
+
