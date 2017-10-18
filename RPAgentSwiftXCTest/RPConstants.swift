ddc7b88e3f0a74c10d61e2b0ccc5ab478610ed2a
Sergey Komarov <sergey.komarov@oxagile.com>
2017-10-18 11:28:08 +0300

    New Apple devices are added

--- RPAgentSwiftXCTest/RPConstants.swift
+++ RPAgentSwiftXCTest/RPConstants.swift
@@ -49,6 +49,9 @@ public extension UIDevice {
         case "iPhone8,2":                               model = "iPhone 6s Plus\(postfix)"
         case "iPhone9,1", "iPhone9,3":                  model = "iPhone 7\(postfix)"
         case "iPhone9,2", "iPhone9,4":                  model = "iPhone 7 Plus\(postfix)"
+        case "iPhone10,1", "iPhone10,4":                model = "iPhone 8\(postfix)"
+        case "iPhone10,2", "iPhone10,5":                model = "iPhone 8 Plus\(postfix)"
+        case "iPhone10,3", "iPhone10,6":                model = "iPhone X\(postfix)"
         case "iPhone8,4":                               model = "iPhone SE\(postfix)"
         case "iPad2,1", "iPad2,2", "iPad2,3", "iPad2,4":model = "iPad 2\(postfix)"
         case "iPad3,1", "iPad3,2", "iPad3,3":           model = "iPad 3\(postfix)"
@@ -63,7 +66,10 @@ public extension UIDevice {
         case "iPad6,7", "iPad6,8":                      model = "iPad Pro 12.9\"\(postfix)"
         case "iPad7,1", "iPad7,2":                      model = "iPad Pro 2 12.9\"\(postfix)"
         case "iPad7,3", "iPad7,4":                      model = "iPad Pro 10.5\"\(postfix)"
-        case "AppleTV5,3":                              model = "Apple TV\(postfix)"
+        case "AppleTV2,1":                              model = "Apple TV 2\(postfix)"
+        case "AppleTV3,1", "AppleTV3,2":                model = "Apple TV 3\(postfix)"
+        case "AppleTV5,3":                              model = "Apple TV 4\(postfix)"
+        case "AppleTV6,2":                              model = "Apple TV 4K\(postfix)"
         default:                                        model = identifier
         }
         return model
